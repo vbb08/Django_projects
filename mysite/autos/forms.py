7339bebce9731dca12b71da40d11930106e63715
Valentina Bresciani Blicharz <valentina.bresciani@gmail.com>
2023-07-12 12:43:38 +0000

    CRUD autos app shortened

--- mysite/autos/forms.py
+++ mysite/autos/forms.py
@@ -1,3 +1,5 @@
+'''
+#NOT needed if in views.py there are classes with CreateView, UpdateView and DeleteView
 from django.forms import ModelForm
 from autos.models import Make
 
@@ -7,3 +9,4 @@ class MakeForm(ModelForm):
     class Meta:
         model = Make
         fields = '__all__'
+'''
