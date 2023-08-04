97faab4f772b4397f5fe8199dcc662ecbeeea790
Valentina Bresciani Blicharz <valentina.bresciani@gmail.com>
2023-08-04 14:33:27 +0000

    Fixing hello app bugs

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -1,18 +1,8 @@
-'''from django.urls import path
-from . import views
-
-app_name = 'hello'
-
-urlpatterns = [
-    path('', views.hello),
-]
-'''
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 
 app_name = 'hello'
 urlpatterns = [
-    path('', views.HelloView.as_view(), name='hello'),
-    path('owner', views.owner, name='owner'),
+#    path('cookie', views.cookie, name='cookie'),
+    path('', views.sessfun, name='sessfun'),
 ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 # https://docs.djangoproject.com/en/3.0/ref/request-response/#django.http.HttpRequest.COOKIES
 # HttpResponse.set_cookie(key, value='', max_age=None, expires=None, path='/',
 #     domain=None, secure=None, httponly=False, samesite=None)
+
+'''
 def cookie(request):
     print(request.COOKIES)
     oldval = request.COOKIES.get('zap', None)
@@ -15,12 +17,15 @@ def cookie(request):
         resp.set_cookie('zap', 42) # No expired date = until browser close
     resp.set_cookie('sakaicar', 42, max_age=1000) # seconds until expire
     return resp
-
+'''
 # https://www.youtube.com/watch?v=Ye8mB6VsUHw
 
 def sessfun(request) :
     num_visits = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_visits
-    if num_visits > 4 : del(request.session['num_visits'])
+    if num_visits > 10 : del(request.session['num_visits'])
     resp = HttpResponse('view count='+str(num_visits))
+    resp.set_cookie("dj4e_cookie", "10f5fbb1", max_age=1000)
     return resp
+
+
